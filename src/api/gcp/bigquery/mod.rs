91dcb73cd12d4fe8871faf371eb5d4f1fce9263b
anudeeps0306 <anudeep0306@gmail.com>
2024-02-10 23:02:04 +0530

    added files

---
+++ src/api/gcp/bigquery/mod.rs
@@ -0,0 +1,6 @@
+use actix_web::{get, HttpResponse};
+
+#[get("/bigquery")]
+pub async fn bigquery_handler() -> HttpResponse {
+    HttpResponse::Ok().body("BigQuery endpoint")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,17 @@
-use actix_web::{web, App, HttpServer, HttpResponse, Responder};
-use std::sync::{Arc, Mutex};
+use actix_web::{HttpServer, App};
+mod api;
+use crate::api::gcp::bigquery::bigquery_handler;
+use crate::api::gcp::vertexai::vertexai_handler;
 
-enum Services {
-    AWS(String),
-    GCP(String),
-    Azure(String),
-}
-
-struct ServiceProvider {
-    service: Option<Services>,
-}
-
-impl ServiceProvider {
-    fn set_service(&mut self, service: Services) {
-        self.service = Some(service);
-    }
-}
-
-async fn set_service_provider(
-    service: web::Json<Services>,
-    data: web::Data<Arc<Mutex<ServiceProvider>>>,
-) -> impl Responder {
-    let mut service_provider = data.lock().unwrap();
-    service_provider.set_service(service.into_inner());
-    "Service provider set".to_string()
-}
-
-async fn test(
-    data: web::Data<Arc<Mutex<ServiceProvider>>>,
-) -> impl Responder {
-    let service_provider = data.lock().unwrap();
-    if let Some(ref service) = service_provider.service {
-        HttpResponse::Ok().body(format!("{:?}", service))
-    } else {
-        HttpResponse::NotFound().body("Service provider not set")
-    }
-}
-
-#[get("/hi")]
-async fn hi() -> impl Responder {
-    HttpResponse::Ok().body("Working fine !")
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let service_provider = Arc::new(Mutex::new(ServiceProvider { service: None }));
-
-    HttpServer::new(move || {
+    HttpServer::new(|| {
         App::new()
-            .app_data(web::Data::new(service_provider.clone()))
-            .route("/set-service", web::post().to(set_service_provider))
-            .service(web::resource("/test").route(web::get().to(test)))
-            .service(hi)
+            .service(bigquery_handler)
+            .service(vertexai_handler)
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
